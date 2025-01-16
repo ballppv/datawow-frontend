3867353f6abcc5574eb94e948e8e05ef7144e07c
Ball Pornpavee <pornpavee.ji@gmail.com>
2025-01-17 04:31:01 +0700

    init commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,49 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
-export default {
+const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/layouts/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/layouts/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      screens: {
+        sm: '320px',
+        mobile: '375px',
+        md: '768px',
+        '2md': '900px',
+        lg: '1024px',
+        xl: '1440px',
+      },
+      fontSize: {
+        md: ['16px', '24px'],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        golden: '#C5A365',
+        success: '#49A569',
+        green: {
+          100: '#D8E9E4',
+          300: '#2B5F44',
+          500: '#243831',
+        },
+        grey: {
+          100: '#BBC2C0',
+          300: '#939494',
+        },
+      },
+      backgroundColor: {},
+      borderColor: {},
+      textColor: {
+        primary: '#191919',
+        secondary: '#A0AFBA',
       },
     },
   },
   plugins: [],
-} satisfies Config;
+}
+export default config
